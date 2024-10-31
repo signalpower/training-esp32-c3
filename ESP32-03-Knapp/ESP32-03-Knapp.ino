090ea783e51bb36ac406845049ed74bd6377093f
areh <1456451+areh@users.noreply.github.com>
2024-10-31 07:37:00 +0100

    Added example code for tasks 3 and 11

---
+++ ESP32-03-Knapp/ESP32-03-Knapp.ino
@@ -0,0 +1,28 @@
+#define BUTTON_PIN 10
+
+//Reverse logic with pullup resistor
+#define BUTTON_PRESSED 0
+#define BUTTON_OPEN    1
+
+int buttonState = 0;
+int counter = 0;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("\n\n* * * * * * * * * *  Button start  * * * * * * * * * *\n\n");
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+}//setup()
+
+void loop() {
+  buttonState = digitalRead(BUTTON_PIN);
+  if (buttonState == BUTTON_PRESSED) {
+    Serial.print("Button pressed! ");
+    counter++;
+    Serial.println(counter);
+    delay(300);
+  }//if
+
+}//loop()
+
+//EOF
