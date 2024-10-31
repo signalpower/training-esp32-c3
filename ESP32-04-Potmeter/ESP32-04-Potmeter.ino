88b3036023e0dc69934ef6716ba18961752e42ba
areh <1456451+areh@users.noreply.github.com>
2024-10-31 07:38:35 +0100

    Added example code for task 4

---
+++ ESP32-04-Potmeter/ESP32-04-Potmeter.ino
@@ -0,0 +1,35 @@
+#define POT_PIN 3
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("\n\n* * * * * * * * * *  Potmeter start  * * * * * * * * * *\n\n");
+  analogSetAttenuation(ADC_11db);
+}//setup()
+
+void loop() {
+  int analogValue = analogRead(POT_PIN);
+  float voltage = floatMap(analogValue, 0, 4095, 0, 3.3);
+  unsigned char byteVal = convertAD(analogValue, 4095);
+
+  Serial.print("Analog: ");
+  Serial.print(analogValue);
+  Serial.print(", Byte: ");
+  Serial.print(byteVal);
+  Serial.print(", Voltage: ");
+  Serial.println(voltage);
+  delay(1000);
+}//loop()
+
+
+float floatMap(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}//floatMap
+
+unsigned char convertAD(int analogVal, int analogMax) {
+  float factor = analogMax / 255;
+  unsigned char returnValue = analogVal/factor;
+  return returnValue;
+}//convertAD
+
+//EOF
