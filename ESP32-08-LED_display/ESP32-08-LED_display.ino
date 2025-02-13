1c2f1dd2a77f22a1d88e1d1afddd381202d63142
Are Hoel <1456451+areh@users.noreply.github.com>
2025-02-13 12:19:41 +0100

    Moved comments to line ends

--- ESP32-08-LED_display/ESP32-08-LED_display.ino
+++ ESP32-08-LED_display/ESP32-08-LED_display.ino
@@ -5,11 +5,11 @@
  *
  */
 
-#include <GyverSegment.h>                       // For LED display (MAX7219)
+#include <GyverSegment.h> // For LED display (MAX7219)
 
-#define DIO_PIN 10                              // 7-segment MAX7219 - DIN pin (data)
-#define CLK_PIN  6                              // 7-segment MAX7219 - CLK pin
-#define LAT_PIN  7                              // 7-segment MAX7219 - CS pin
+#define DIO_PIN 10        // 7-segment MAX7219 - DIN pin (data)
+#define CLK_PIN  6        // 7-segment MAX7219 - CLK pin
+#define LAT_PIN  7        // 7-segment MAX7219 - CS pin
 
 Disp7219<1> disp(DIO_PIN, CLK_PIN, LAT_PIN);    // 7-segment display, from GyverSegment library
 
@@ -21,38 +21,22 @@ void setup() {
 }//setup
 
 void loop() {
-  // Clear display buffer
-  disp.clear();
-  // Reset cursor (left)
-  disp.home();
-  // Print text
-  disp.print("HELLO");
-  // Update display to show content
-  disp.update();
+  disp.clear();           // Clear display buffer
+  disp.home();            // Reset cursor (left)
+  disp.print("HELLO");    // Print text
+  disp.update();          // Update display to show content
   delay(3000);
-
-  // Clear display buffer
-  disp.clear();
-  // Reset cursor (left)
-  disp.home();
-  // Print integer
-  disp.print(23456789);
-  // Update display to show content
-  disp.update();
+  disp.clear();           // Clear display buffer
+  disp.home();            // Reset cursor (left)
+  disp.print(23456789);   // Print integer
+  disp.update();          // Update display to show content 
   delay(3000);
-
-  // Clear display buffer
-  disp.clear();
-  // Set cursor at end (right hand side)
-  disp.setCursorEnd();
-  // Print from the right
-  disp.printRight(false);
-  // Print float (decimal) number
-  disp.print(123.45);
-  // Update display to show content
-  disp.update();
+  disp.clear();           // Clear display buffer
+  disp.setCursorEnd();    // Set cursor at end (right hand side)
+  disp.printRight(false); // Print from the right
+  disp.print(123.45);     // Print float (decimal) number 
+  disp.update();          // Update display to show content 
   delay(1000);
-
 }//loop
 
 //EOF
