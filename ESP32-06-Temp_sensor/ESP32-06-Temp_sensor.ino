390881a4d76eb5376231128b50dba5be12581a91
areh <1456451+areh@users.noreply.github.com>
2025-02-16 16:03:55 +0100

    Updated task 6 schematics and code

--- ESP32-06-Temp_sensor/ESP32-06-Temp_sensor.ino
+++ ESP32-06-Temp_sensor/ESP32-06-Temp_sensor.ino
@@ -12,7 +12,7 @@
 #define TEMPSENSOR_ONEWIRE 10
 
 OneWire tempBus(TEMPSENSOR_ONEWIRE);
-DallasTemperature tempSensors(tempBus);
+DallasTemperature tempSensors(&tempBus);
 
 void setup() {
   Serial.begin(115200);
@@ -26,7 +26,7 @@ void loop() {
   tempSensors.requestTemperatures();
   Serial.print("Celsius temperature: ");
   Serial.println(tempSensors.getTempCByIndex(0)); //Get temperature from the first sensor on the bus
-  delay(1000): //Loop wait
+  delay(1000); //Loop wait
 }//loop
 
 //EOF
